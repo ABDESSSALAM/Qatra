8e58c35c9edd08a6cb749a515fd303e9752320c1
abdessalam <abdessalam.ait.999@gmail.com>
2022-06-14 23:55:01 +0100

    waiting dynamic done

--- public/js/app.js
+++ public/js/app.js
@@ -6762,38 +6762,107 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
 /* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
 /* harmony export */ });
-/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
-/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react/jsx-runtime */ "./node_modules/react/jsx-runtime.js");
+/* harmony import */ var _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @babel/runtime/regenerator */ "./node_modules/@babel/runtime/regenerator/index.js");
+/* harmony import */ var _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react_router_dom__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! react-router-dom */ "./node_modules/react-router/index.js");
+/* harmony import */ var _Context_UserContext__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../Context/UserContext */ "./resources/Context/UserContext.js");
+/* harmony import */ var _CONF_AXIOS__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ../CONF_AXIOS */ "./resources/js/CONF_AXIOS.js");
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! react/jsx-runtime */ "./node_modules/react/jsx-runtime.js");
+
+
+function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
+
+function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
+
+function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest(); }
+
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
+function _iterableToArrayLimit(arr, i) { var _i = arr == null ? null : typeof Symbol !== "undefined" && arr[Symbol.iterator] || arr["@@iterator"]; if (_i == null) return; var _arr = []; var _n = true; var _d = false; var _s, _e; try { for (_i = _i.call(arr); !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
+
+function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
+
+
+
+
 
 
 
 
 function WaitAssoc() {
-  return /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)("div", {
+  var _useContext = (0,react__WEBPACK_IMPORTED_MODULE_1__.useContext)(_Context_UserContext__WEBPACK_IMPORTED_MODULE_2__.UserContext),
+      user = _useContext.user,
+      setUser = _useContext.setUser;
+
+  var _useState = (0,react__WEBPACK_IMPORTED_MODULE_1__.useState)(false),
+      _useState2 = _slicedToArray(_useState, 2),
+      logout = _useState2[0],
+      setLogout = _useState2[1];
+
+  var LogOut = /*#__PURE__*/function () {
+    var _ref = _asyncToGenerator( /*#__PURE__*/_babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0___default().mark(function _callee() {
+      return _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0___default().wrap(function _callee$(_context) {
+        while (1) {
+          switch (_context.prev = _context.next) {
+            case 0:
+              _context.next = 2;
+              return _CONF_AXIOS__WEBPACK_IMPORTED_MODULE_3__["default"].post('logout').then(function (res) {
+                if (res.data.message == "Success") {
+                  setLogout(true);
+                }
+              });
+
+            case 2:
+            case "end":
+              return _context.stop();
+          }
+        }
+      }, _callee);
+    }));
+
+    return function LogOut() {
+      return _ref.apply(this, arguments);
+    };
+  }();
+
+  if (logout) {
+    setUser({});
+    return /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx)(react_router_dom__WEBPACK_IMPORTED_MODULE_5__.Navigate, {
+      to: "/"
+    });
+  }
+
+  return /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx)("div", {
     className: "w-screen h-screen flex flex-col justify-center items-center bg-yellow-300",
-    children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsxs)("div", {
+    children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsxs)("div", {
       className: "flex flex-col h-1/3 items-center",
-      children: [/*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsxs)("p", {
+      children: [/*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsxs)("p", {
         className: "text-4xl font-semibold text-black",
-        children: ["Bon jour , ", /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)("span", {
+        children: ["Bon jour , ", /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx)("span", {
           className: "font-bold capitalize",
-          children: "prenom"
+          children: user.prenom + ' ' + user.nom
         }), " "]
-      }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsxs)("div", {
+      }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsxs)("div", {
         className: "mt-5 flex items-end",
-        children: [/*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)("i", {
+        children: [/*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx)("i", {
           className: "fa-solid fa-hospital-user text-6xl mx-2"
-        }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)("p", {
+        }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx)("p", {
           className: "text-lg ",
           children: "Votre compte d'association n'est pas encore v\xE9rifier,Merci!!"
         })]
-      }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsxs)("p", {
+      }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsxs)("p", {
         className: "text-lg ",
-        children: ["contacter nous en ", /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)("span", {
+        children: ["contacter nous en ", /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx)("span", {
           className: "font-bold",
           children: "contact@qatra.ma"
         })]
-      }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)("button", {
+      }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx)("button", {
+        onClick: LogOut,
         className: "bg-secondary text-white self-stretch mt-5 rounded-xl shadow-2xl text-xl py-1 hover:text-gray-300",
         children: "d\xE9connecter"
       })]
--- resources/js/pages/WaitAssoc.jsx
+++ resources/js/pages/WaitAssoc.jsx
@@ -1,16 +1,35 @@
-import React from 'react'
-
+import React,{useContext,useState} from 'react'
+import { Navigate } from 'react-router-dom';
+import { UserContext } from '../../Context/UserContext'
+import axios_api from '../CONF_AXIOS';
 function WaitAssoc() {
+  
+  const {user,setUser}=useContext(UserContext);
+  const [logout,setLogout]=useState(false)
+  const LogOut = async ()=>{
+    await axios_api.post('logout')
+    .then(res=>{
+      if( res.data.message=="Success"){
+          setLogout(true);
+      }
+       
+    })
+  }
+  if(logout){
+    setUser({})
+    return <Navigate to='/' />
+
+  }
   return (
     <div className='w-screen h-screen flex flex-col justify-center items-center bg-yellow-300'>
         <div className='flex flex-col h-1/3 items-center'>
-          <p className='text-4xl font-semibold text-black'>Bon jour , <span className='font-bold capitalize'>prenom</span> </p> 
+          <p className='text-4xl font-semibold text-black'>Bon jour , <span className='font-bold capitalize'>{user.prenom + ' ' + user.nom}</span> </p> 
           <div className='mt-5 flex items-end'>
             <i className="fa-solid fa-hospital-user text-6xl mx-2"></i>
             <p className='text-lg '>Votre compte d'association n'est pas encore vérifier,Merci!!</p>
           </div>
           <p className='text-lg '>contacter nous en <span className='font-bold'>contact@qatra.ma</span></p>
-          <button className='bg-secondary text-white self-stretch mt-5 rounded-xl shadow-2xl text-xl py-1 hover:text-gray-300'>déconnecter</button>
+          <button onClick={LogOut} className='bg-secondary text-white self-stretch mt-5 rounded-xl shadow-2xl text-xl py-1 hover:text-gray-300'>déconnecter</button>
         </div>
         
 
